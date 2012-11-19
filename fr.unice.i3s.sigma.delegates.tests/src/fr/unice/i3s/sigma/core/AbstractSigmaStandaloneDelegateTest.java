cc0f127b03992475198cac6858a8ef288a0cd146
Filip Krikava <filip.krikava@i3s.unice.fr>
2012-11-19 15:02:57 +0100

    Minor changes in the abstract test

--- fr.unice.i3s.sigma.delegates.tests/src/fr/unice/i3s/sigma/core/AbstractSigmaStandaloneDelegateTest.java
+++ fr.unice.i3s.sigma.delegates.tests/src/fr/unice/i3s/sigma/core/AbstractSigmaStandaloneDelegateTest.java
@@ -1,6 +1,5 @@
 package fr.unice.i3s.sigma.core;
 
-import org.eclipse.emf.ecore.EPackage;
 import org.junit.BeforeClass;
 
 import testmodel.TestClass;
@@ -15,12 +14,9 @@ public abstract class AbstractSigmaStandaloneDelegateTest {
 	@BeforeClass
 	public static void registerDelegates() {
 		// load package
-		EPackage.Registry.INSTANCE.put(TmPackage.eNS_URI, TmPackage.eINSTANCE);
+		TmPackage.eINSTANCE.eClass();
 
-		// // initialize resource factories
-		// Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
-		// "xmi", new XMIResourceFactoryImpl());
-
+		// register Sigma delegate
 		SigmaDelegateDomain.installGlobally();
 	}
 
@@ -29,12 +25,5 @@ public abstract class AbstractSigmaStandaloneDelegateTest {
 		tc.setAttribute(attribute);
 		return tc;
 	}
-
-	// protected static Library load(String filename) {
-	// ResourceSet rs = new ResourceSetImpl();
-	// Resource r = rs.getResource(
-	// URI.createFileURI(new File(filename).getAbsolutePath()), true);
-	// return (Library) r.getContents().get(0);
-	// }
 
 }
