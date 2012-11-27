a65ef3dd7c39e08ab55b133eedabc7b6e74b43e2
Filip Krikava <filip.krikava@i3s.unice.fr>
2012-11-27 18:34:50 +0100

    Added full test of the scala score extension generator

---
+++ fr.unice.i3s.sigma.scala.tests/src/fr/unice/i3s/sigma/scala/Activator.scala
@@ -0,0 +1,20 @@
+package fr.unice.i3s.sigma.scala
+
+import org.osgi.framework.BundleActivator
+import org.osgi.framework.BundleContext
+
+object Activator {
+  var context: Option[BundleContext] = None
+}
+
+class Activator extends BundleActivator {
+
+  override def start(context: BundleContext) {
+    Activator.context = Some(context)
+  }
+
+  override def stop(context: BundleContext) {
+    Activator.context = None
+  }
+
+}
