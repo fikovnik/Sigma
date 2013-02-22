c9c2824c9a44dacb65157ffc6b5b49d755b1f6f2
Filip Krikava <filip.krikava@i3s.unice.fr>
2013-02-22 20:16:59 +0100

    Scala library example codeine + scala emf support

---
+++ fr.unice.i3s.sigma.examples.scala.library/src-gen/fr/unice/i3s/sigma/examples/library/impl/MemberImpl.java
@@ -0,0 +1,359 @@
+/**
+ */
+package fr.unice.i3s.sigma.examples.library.impl;
+
+import fr.unice.i3s.sigma.examples.library.Book;
+import fr.unice.i3s.sigma.examples.library.Library;
+import fr.unice.i3s.sigma.examples.library.LibraryPackage;
+import fr.unice.i3s.sigma.examples.library.Loan;
+import fr.unice.i3s.sigma.examples.library.Member;
+import fr.unice.i3s.sigma.examples.library.MembershipType;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Member</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link fr.unice.i3s.sigma.examples.library.impl.MemberImpl#getName <em>Name</em>}</li>
+ *   <li>{@link fr.unice.i3s.sigma.examples.library.impl.MemberImpl#getLibrary <em>Library</em>}</li>
+ *   <li>{@link fr.unice.i3s.sigma.examples.library.impl.MemberImpl#getLoans <em>Loans</em>}</li>
+ *   <li>{@link fr.unice.i3s.sigma.examples.library.impl.MemberImpl#getBooks <em>Books</em>}</li>
+ *   <li>{@link fr.unice.i3s.sigma.examples.library.impl.MemberImpl#getMembershipType <em>Membership Type</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class MemberImpl extends EObjectImpl implements Member {
+	/**
+	 * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String NAME_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected String name = NAME_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getMembershipType() <em>Membership Type</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getMembershipType()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final MembershipType MEMBERSHIP_TYPE_EDEFAULT = MembershipType.STANDARD;
+
+	/**
+	 * The cached value of the '{@link #getMembershipType() <em>Membership Type</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getMembershipType()
+	 * @generated
+	 * @ordered
+	 */
+	protected MembershipType membershipType = MEMBERSHIP_TYPE_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected MemberImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return LibraryPackage.Literals.MEMBER;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setName(String newName) {
+		String oldName = name;
+		name = newName;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, LibraryPackage.MEMBER__NAME, oldName, name));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Library getLibrary() {
+		if (eContainerFeatureID() != LibraryPackage.MEMBER__LIBRARY) return null;
+		return (Library)eContainer();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetLibrary(Library newLibrary, NotificationChain msgs) {
+		msgs = eBasicSetContainer((InternalEObject)newLibrary, LibraryPackage.MEMBER__LIBRARY, msgs);
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setLibrary(Library newLibrary) {
+		if (newLibrary != eInternalContainer() || (eContainerFeatureID() != LibraryPackage.MEMBER__LIBRARY && newLibrary != null)) {
+			if (EcoreUtil.isAncestor(this, newLibrary))
+				throw new IllegalArgumentException("Recursive containment not allowed for " + toString());
+			NotificationChain msgs = null;
+			if (eInternalContainer() != null)
+				msgs = eBasicRemoveFromContainer(msgs);
+			if (newLibrary != null)
+				msgs = ((InternalEObject)newLibrary).eInverseAdd(this, LibraryPackage.LIBRARY__MEMBERS, Library.class, msgs);
+			msgs = basicSetLibrary(newLibrary, msgs);
+			if (msgs != null) msgs.dispatch();
+		}
+		else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, LibraryPackage.MEMBER__LIBRARY, newLibrary, newLibrary));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Loan> getLoans() {
+		// TODO: implement this method to return the 'Loans' reference list
+		// Ensure that you remove @generated or mark it @generated NOT
+		// The list is expected to implement org.eclipse.emf.ecore.util.InternalEList and org.eclipse.emf.ecore.EStructuralFeature.Setting
+		// so it's likely that an appropriate subclass of org.eclipse.emf.ecore.util.EcoreEList should be used.
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Book> getBooks() {
+		// TODO: implement this method to return the 'Books' reference list
+		// Ensure that you remove @generated or mark it @generated NOT
+		// The list is expected to implement org.eclipse.emf.ecore.util.InternalEList and org.eclipse.emf.ecore.EStructuralFeature.Setting
+		// so it's likely that an appropriate subclass of org.eclipse.emf.ecore.util.EcoreEList should be used.
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public MembershipType getMembershipType() {
+		return membershipType;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setMembershipType(MembershipType newMembershipType) {
+		MembershipType oldMembershipType = membershipType;
+		membershipType = newMembershipType == null ? MEMBERSHIP_TYPE_EDEFAULT : newMembershipType;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, LibraryPackage.MEMBER__MEMBERSHIP_TYPE, oldMembershipType, membershipType));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseAdd(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case LibraryPackage.MEMBER__LIBRARY:
+				if (eInternalContainer() != null)
+					msgs = eBasicRemoveFromContainer(msgs);
+				return basicSetLibrary((Library)otherEnd, msgs);
+		}
+		return super.eInverseAdd(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case LibraryPackage.MEMBER__LIBRARY:
+				return basicSetLibrary(null, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eBasicRemoveFromContainerFeature(NotificationChain msgs) {
+		switch (eContainerFeatureID()) {
+			case LibraryPackage.MEMBER__LIBRARY:
+				return eInternalContainer().eInverseRemove(this, LibraryPackage.LIBRARY__MEMBERS, Library.class, msgs);
+		}
+		return super.eBasicRemoveFromContainerFeature(msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case LibraryPackage.MEMBER__NAME:
+				return getName();
+			case LibraryPackage.MEMBER__LIBRARY:
+				return getLibrary();
+			case LibraryPackage.MEMBER__LOANS:
+				return getLoans();
+			case LibraryPackage.MEMBER__BOOKS:
+				return getBooks();
+			case LibraryPackage.MEMBER__MEMBERSHIP_TYPE:
+				return getMembershipType();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case LibraryPackage.MEMBER__NAME:
+				setName((String)newValue);
+				return;
+			case LibraryPackage.MEMBER__LIBRARY:
+				setLibrary((Library)newValue);
+				return;
+			case LibraryPackage.MEMBER__MEMBERSHIP_TYPE:
+				setMembershipType((MembershipType)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case LibraryPackage.MEMBER__NAME:
+				setName(NAME_EDEFAULT);
+				return;
+			case LibraryPackage.MEMBER__LIBRARY:
+				setLibrary((Library)null);
+				return;
+			case LibraryPackage.MEMBER__MEMBERSHIP_TYPE:
+				setMembershipType(MEMBERSHIP_TYPE_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case LibraryPackage.MEMBER__NAME:
+				return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+			case LibraryPackage.MEMBER__LIBRARY:
+				return getLibrary() != null;
+			case LibraryPackage.MEMBER__LOANS:
+				return !getLoans().isEmpty();
+			case LibraryPackage.MEMBER__BOOKS:
+				return !getBooks().isEmpty();
+			case LibraryPackage.MEMBER__MEMBERSHIP_TYPE:
+				return membershipType != MEMBERSHIP_TYPE_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (name: ");
+		result.append(name);
+		result.append(", membershipType: ");
+		result.append(membershipType);
+		result.append(')');
+		return result.toString();
+	}
+
+} //MemberImpl
