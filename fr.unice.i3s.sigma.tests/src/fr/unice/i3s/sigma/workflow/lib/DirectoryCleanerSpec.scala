6f4b147313ce64cb04e683e1d882a542900e548a
Filip Krikava <filip.krikava@i3s.unice.fr>
2013-03-19 00:36:54 +0100

    Added DirectoryCleaner

---
+++ fr.unice.i3s.sigma.tests/src/fr/unice/i3s/sigma/workflow/lib/DirectoryCleanerSpec.scala
@@ -0,0 +1,26 @@
+package fr.unice.i3s.sigma.workflow.lib
+
+import java.io.File
+import org.junit.runner.RunWith
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.MustMatchers
+import fr.unice.i3s.sigma.scala.workflow.lib.DirectoryCleaner
+import fr.unice.i3s.sigma.util.IOUtils
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class DirectoryCleanerSpec extends FlatSpec with MustMatchers {
+
+  "DirectoryCleaner" must "remove all files within a direcoty" in {
+    val tmp = IOUtils.mkdtemp
+    new File(tmp, "1/2").mkdirs must be === true
+    new File(tmp, "1/2/a").createNewFile must be === true
+    new File(tmp, "1/2/b").createNewFile must be === true
+    new File(tmp, "3").mkdirs must be === true
+    new File(tmp, "3/c").createNewFile must be === true
+
+    DirectoryCleaner(tmp.getCanonicalPath)
+
+  }
+
+}
